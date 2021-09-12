208787c71c62474d18b7752361f310d25e730f18
CecilebougmaIstic <cecile.bougma@etudiant.univ-rennes1.fr>
2021-09-13 00:57:33 +0200

    first commit

---
+++ src/test/java/fr/istic/mnmtpun/AppTest.java
@@ -0,0 +1,24 @@
+package fr.istic.mnmtpun;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void shoulReturnTrueIfADocumentAdd() {
+        assertTrue("document", true);
+    }
+}
